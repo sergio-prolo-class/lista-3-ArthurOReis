cdeeada938401de180e95e0a1d12ac88fe242aa0
ArthurOReis <arthuroreis2005@gmail.com>
2025-05-15 09:03:14 -0300

    Criando estrutura base para usuário

--- app/src/main/java/ifsc/poo/Classes/ExercicioUm/Usuario.java
+++ app/src/main/java/ifsc/poo/Classes/ExercicioUm/Usuario.java
@@ -1,5 +1,27 @@
 package ifsc.poo.Classes.ExercicioUm;
 
 public class Usuario {
-    
+    private String login;
+    private String senha;
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public String getLogin(){
+        return this.login;
+    }
+
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    }
 }
